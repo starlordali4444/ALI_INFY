a548b8639924b5e5806b6b560f29ecfbf6218c2f
Siraj Ali <starlordali@gmail.com>
2020-04-01 11:35:10 +0530

    April 1

---
+++ GENERIC/PROGRAM/for.py
@@ -0,0 +1,19 @@
+# For Loop
+
+# for i in 1,2,3,4,5:
+#     print(i)
+
+# sum=0
+# for i in 1,2,3,4,5:
+#     sum+=i
+#     print(sum)
+
+# Use of Range
+# for i in range(1,6):
+#     print(i)
+
+# Another use of Range Function
+# Three parameter 1= start 10 = end , 2 = increment value
+for i in range(1,10,2):
+    # print(i)
+    print(i,end="") # prints in same line
---
+++ GENERIC/PROGRAM/while.py
@@ -0,0 +1,4 @@
+#WAP to take input untill alpha is given
+a=""
+while (a!="alpha"):
+    a=input("Enter a word")
